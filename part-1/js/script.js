052423c8e77d26721d675a92fe73abeecf05b92c
arieldavis22 <aadavisbusiness@gmail.com>
2020-07-12 11:30:01 -0400

    css selectors

--- part-1/js/script.js
+++ part-1/js/script.js
@@ -1,13 +1,11 @@
 $(function () {
-  // $('p').css("background-color", "rgba(180, 180, 30, 0.8)")
-
-  // $("input[type='text']").css("background-color", "rgba(180, 180, 30, 0.8)")
-
-  // $("li:first").css("background-color", "rgba(180, 180, 30, 0.8)")
-  // $("li:last").css("background-color", "rgba(180, 180, 30, 0.8)")
-  // $("li:even").css("background-color", "rgba(180, 180, 30, 0.8)")
-  // $("li:odd").css("background-color", "rgba(180, 180, 30, 0.8)")
-  
+  // $("#list").find("li").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("#list").children("li").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("#list").parents("div").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("#list").parent().css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("#list").siblings(":header").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("#list").prev().css("background-color", "rgba(180, 180, 30, 0.8)")
+  $("#list").next().css("background-color", "rgba(180, 180, 30, 0.8)")
 });
 
 
@@ -105,9 +103,19 @@ $(function () {
 
 //============================================================================================
 
-$('.lightbox').delay(500).fadeIn(1000).delay(500).fadeOut(1000)
+// $('.lightbox').delay(500).fadeIn(1000).delay(500).fadeOut(1000)
 
 //============================================================================================
+
+  // $('p').css("background-color", "rgba(180, 180, 30, 0.8)")
+
+  // $("input[type='text']").css("background-color", "rgba(180, 180, 30, 0.8)")
+
+  // $("li:first").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("li:last").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("li:even").css("background-color", "rgba(180, 180, 30, 0.8)")
+  // $("li:odd").css("background-color", "rgba(180, 180, 30, 0.8)")
+
 //============================================================================================
 //============================================================================================
 //============================================================================================
