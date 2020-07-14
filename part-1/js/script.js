d41ffec9de44d23056bad123b94d14a685334439
Ariel Davis <adavis@interplaylearning.com>
2020-07-13 22:19:57 -0400

    changing css props

--- part-1/js/script.js
+++ part-1/js/script.js
@@ -1,20 +1,5 @@
 $(function () {
-  let galleryImage = $(".gallery").find("img").first()
 
-  const images = [
-    "images/laptop-mobile_small.jpg",
-    "images/laptop-on-table_small.jpg",
-    "images/people-office-group-team_small.jpg",    
-  ]
-
-  let i = 0
-  setInterval(() => {
-    i = (i + 1) % images.length // 0, 1, 2, 0, 1, 2, 0, 1, 2....
-    galleryImage.fadeOut(function() {
-      $(this).attr("src", images[i])
-      $(this).fadeIn()
-    })
-  }, 2000)
 
 });
 
@@ -210,7 +195,40 @@ $(function () {
   // input.val("test haha")
 
 //============================================================================================
+
+// let galleryImage = $(".gallery").find("img").first()
+
+// const images = [
+//   "images/laptop-mobile_small.jpg",
+//   "images/laptop-on-table_small.jpg",
+//   "images/people-office-group-team_small.jpg",    
+// ]
+
+// let i = 0
+// setInterval(() => {
+//   i = (i + 1) % images.length // 0, 1, 2, 0, 1, 2, 0, 1, 2....
+//   galleryImage.fadeOut(function() {
+//     $(this).attr("src", images[i])
+//     $(this).fadeIn()
+//   })
+// }, 2000)
+
 //============================================================================================
+
+// $(".gallery").css("display", "none") //.hide()
+
+// const redBox = $(".red-box")
+// console.log(redBox.css("width"))
+
+// redBox.css("background-color", "#AA7700")
+// $("p").css("font-size", "18px")
+// redBox.css("width", "+=20px")
+
+// const properties = $("p").css(["font-size", "line-height", "color"])
+// console.log(properties);
+
+// redBox.css("user-select", "none")
+
 //============================================================================================
 //============================================================================================
 //============================================================================================
